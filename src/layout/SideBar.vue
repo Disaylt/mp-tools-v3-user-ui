8adf3c511c88a7491170e0e03eb9b11a6d8df138
Uladzislau Krupski <uladzislau.krupski@alreadymedia.com>
2024-12-23 18:17:34 +0300

    feat: add sidebar

--- src/layout/SideBar.vue
+++ src/layout/SideBar.vue
@@ -1,14 +1,31 @@
 <template>
     <div class="p-2">
-        <p v-for="n in 10">{{ n }}</p>
+        <Select v-model="selectedCity" :options="cities" optionLabel="name" placeholder="Select a City" class="w-full md:w-56" />
     </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import Select from 'primevue/select';
 
 
 export default defineComponent({
+    components:{
+        Select
+    },
+    data() {
+        return {
+            selectedCity: null,
+            isDarkMode : true,
+            cities: [
+                { name: 'New York', code: 'NY' },
+                { name: 'Rome', code: 'RM' },
+                { name: 'London', code: 'LDN' },
+                { name: 'Istanbul', code: 'IST' },
+                { name: 'Paris', code: 'PRS' }
+            ]
+        };
+    }
 });
 </script>
 
