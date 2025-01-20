1acefc634c195fb489ad22a21ced10d64027135b
Disaylt <disofban@gmail.com>
2025-01-21 01:12:59 +0300

    feat: change endpoints

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -11,13 +11,13 @@ interface IUserServcie{
 
 class UserServcie implements IUserServcie{
     logout(): Promise<AxiosResponse> {
-        return apiService.put("/identity/v1/user/logout")
+        return apiService.put("/auth/v1/user/logout")
     }
     login(data : Login): Promise<AxiosResponse<AuthResult>> {
-        return apiService.post<AuthResult>("/identity/v1/user/login", data);
+        return apiService.post<AuthResult>("/auth/v1/user/login", data);
     }
     register(newUser : NewUser): Promise<AxiosResponse<AuthResult>> {
-        return apiService.post<AuthResult>("/identity/v1/user/register", newUser);
+        return apiService.post<AuthResult>("/auth/v1/user/register", newUser);
     }
     getInfo(): Promise<AxiosResponse<IdentityDetails>>{
         return apiService.get<IdentityDetails>("/identity/v1/user/details");
